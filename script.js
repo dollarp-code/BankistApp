074a08840e5aa455d62e55ee88284f402e323842
dolapo <husseindollarp@yahoo.com>
2024-05-11 17:39:35 -0700

    Adding Event listeners

--- script.js
+++ script.js
@@ -77,36 +77,32 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => (acc += mov), 0);
   labelBalance.textContent = `${balance}€`;
 };
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = interest;
 };
-
-calcDisplaySummary(account1.movements);
 
 // CREATING THE LOGIN USERNAMES
 const createUsernames = function (accts) {
@@ -139,12 +135,18 @@ btnLogin.addEventListener('click', function (e) {
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
     }`;
-    containerApp.style.opacity = '100';
+    containerApp.style.opacity = 100;
+
+    // clear input field
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
 
     // Display movements
-    displayMovements(currentAccount);
+    displayMovements(currentAccount.movements);
+
     // Display balance
-    calcDisplayBalance(currentAccount);
+    calcDisplayBalance(currentAccount.movements);
+
     // Display Summary
     calcDisplaySummary(currentAccount);
   }
